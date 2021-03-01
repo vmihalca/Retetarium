caaab7832eafbb9e751661d7d971d7a34a13a631
Vlad Mihalca <vmihalca@yahoo.com>
2021-03-01 19:50:21 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.carousel-header .splide', {
+        'cover' : true,
+        'heightRatio' : 0.3,
+	    rewind: true,
+        type    : 'fade',
+        autoplay: true,
+        interval: 3500,
+        pauseOnFocus: true
+    } ).mount();
+} );
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide('.carousel-circle .splide', {
+        type   : 'loop',
+	    perPage: 4,
+	    perMove: 1,
+    } ).mount();
+} );
+
+const myNav = document.querySelector('nav');
+
+function makeChangeNav(e) {
+    const scrollValue = window.scrollY;
+
+    if(scrollValue > 0) {
+        myNav.classList.add('sticky');
+    }
+    else {
+        myNav.classList.remove('sticky');
+    }
+}
+window.addEventListener('scroll',makeChangeNav);
